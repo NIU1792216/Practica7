0e73ad90607dbf571f7abb0175858d7a54f09045
NIU1792216 <1792216@uab.cat>
2026-04-22 16:47:30 +0200

    Pr7.c Acabada

--- Pr7.c
+++ Pr7.c
@@ -1,13 +1,17 @@
+/*
+ Marco Rondón Torres, 1794984
+ Mario Roda Sevilla, 1792216
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct vertex{
-    unsigned id, num_fills;
-    struct vertex * fills[4], *pare;
+    unsigned id, num_fills, fills[4]; // encara que digem fills, ens referim a la valencia i a quins nodes esta conectat cada vertex
+    struct vertex *pare;
 } vertex;
 
-typedef struct{
-    vertex * siguiente;
+typedef struct Element{
+    struct Element * siguiente;
     vertex * mateix;
 }ElementPila;
 
@@ -16,27 +20,29 @@ typedef struct pila{
 }pila;
 
 void apilar(pila * p, vertex * v);
+vertex * desapilar(pila * p);
 
 int main(){
     FILE * fitxer;
-    char num_vertex=0, i, j, *estat_vertex, caracter;
-    vertex ** llista, * anterior, * actual;
+    unsigned i, j, vertex_cami=0, num_vertex=0;
+    char * estat_vertex, caracter;
+    vertex ** llista, * actual, * fill, **cami_trobat=NULL;
     pila pila = {NULL};
 
-
-    fitxer = fopen("laberint.txt","r");
+    fitxer = fopen("./laberint.txt","r");
     if (fitxer == NULL){
         printf("\nNo s'ha accedit al fitxer de dades\n");
         return 1;
     }
-    while (caracter = fgetc(fitxer) != EOF){
+    while ((caracter = fgetc(fitxer)) != EOF){
         if (caracter == '\n'){
             num_vertex++;
         }
     }
     rewind(fitxer);
+    printf("Hi ha %u vertex al graf\n", num_vertex);
     // ------------- Reservem memoria --------------------
-    if ((estat_vertex = (char *)malloc(sizeof(char)*num_vertex)) == NULL){
+    if ((estat_vertex = (char *)calloc(num_vertex, sizeof(char))) == NULL){
         printf("ERROR: No s'ha pogut assignar memoria per falta d'espai");
         return 2;
     }
@@ -50,28 +56,63 @@ int main(){
             return 2;
         }
     }
+    printf("Memoria reservada correctament\n");
     // ------------------- llegim fitxer ----------------
-
     for (i=0;i<num_vertex;i++){
-        fscanf("%u",llista[0]->id);
-        j = 0;
-        while (caracter = fgetc(fitxer) != '\n'){
-            fscanf(", %u",llista[i]->fills[j]);
+        llista[i] -> id = i;
+        j = 0; // j representa el nombre de conexions
+        fscanf(fitxer, "%u",&(llista[i]->fills[j]));
+        j++;
+        while ((caracter = fgetc(fitxer)) != '\n'){
+            fscanf(fitxer," %u",&(llista[i]->fills[j]));
             j++;
         }
         llista[i]->num_fills = j;
     }
     fclose(fitxer);
-    // Creem arbre
+    printf("Informacio del fitxer guardada en estructures de dades\n");
+    // ------------- DFS -----------------------
+    // Posem el pare del primer node com a NULL per quan trobem un cami, poder recorre'l
+    // a la inversa i saber quan ha acabat.
     llista[0]->pare = NULL;
     apilar(&pila, llista[0]);
-    i = 0;
-    while ((i < num_vertex) && (actual->id != num_vertex)){
-        actual = llista[i];
+    // Recorrem els nodes fins que els hem recorregut tots (la pila es queda buida) o
+    // fins que arribem al node de sortida, que en el nostre cas es l'ultim node (55)
+    while ((pila.inici != NULL) && (actual->id != 55)){
+        actual = desapilar(&pila);
+        estat_vertex[actual->id] = 1;
         for (j=0;j < (actual->num_fills); j++){
-            apilar(&pila, actual);
+            // L'us de la variable fill es unicament per fer mes entendible el codi
+            fill = llista[(actual->fills[j])];
+            if (estat_vertex[fill->id] == 0){
+                fill->pare = actual;
+                apilar(&pila, fill);
+            }
         }
     }
+    // Aqui o em trobat la sortida (seguint un cami que podem recorrer a la inversa)
+    // o em recorregut tots els vertex.
+    if (pila.inici == NULL){
+        printf("No s'ha trobat cap cami per sortir del laberint\n");
+        return 0;
+    }
+    // Guardem el cami seguit en una llista (d'apuntadors de vertexs)
+    while (actual != NULL){
+        if ((cami_trobat = (vertex **)realloc(cami_trobat, sizeof(vertex *)*(vertex_cami+1)))==NULL){
+            printf("ERROR: No s'ha pogut assignar memoria per falta d'espai");
+            return 2;
+        }
+        cami_trobat[vertex_cami] = actual;
+        actual = actual->pare;
+        vertex_cami++;
+    }
+    // Mostrem el camí seguit per pantalla
+    printf("El cami trobat es:\n");
+    for (i=(vertex_cami-1);i>0;i--){
+        printf("%u -> ", cami_trobat[i]->id);
+    }
+    printf("%u", cami_trobat[0]->id);
+    return 0;
 }
 
 void apilar(pila * p, vertex * v){
@@ -87,5 +128,7 @@ vertex * desapilar(pila * p){
     }
     ElementPila * e = p->inici;
     p->inici = e->siguiente;
-    return e->mateix;
+    vertex * v = e->mateix;
+    e = NULL;
+    return v;
 }
